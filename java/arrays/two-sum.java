71be566b424da40617595ce8304b34e6ec9842d0
Sharma S <sharmas@karunya.edu.in>
2026-03-25 19:47:24 +0530

    Update two-sum.java

--- java/arrays/two-sum.java
+++ java/arrays/two-sum.java
@@ -1,1 +1,29 @@
+// LeetCode #1 - Two Sum
+// Topic: Arrays, HashMap
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+// Example:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
